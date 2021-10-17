886eecb5bd62ba93e196bcf35ec7bbdbdaee9aa4
Stamir36 <stanislav.miroshnichenko@nure.ua>
2021-10-17 12:31:49 +0300

    Created a Registration and Login form.

---
+++ Website/assets/js/main.js
@@ -0,0 +1,92 @@
+const devices = new RegExp('Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini', "i");
+  
+if (devices.test(navigator.userAgent))
+{
+	 //События для телефонов
+}
+else
+{
+	 //События для ПК
+	 document.getElementById("signInMOBILE").remove();
+	 document.getElementById("signUpMOBILE").remove();
+}
+
+/*===== EXPANDER MENU  =====*/ 
+const showMenu = (toggleId, navbarId, bodyId)=>{
+  const toggle = document.getElementById(toggleId),
+  navbar = document.getElementById(navbarId),
+  bodypadding = document.getElementById(bodyId)
+
+  if(toggle && navbar){
+    toggle.addEventListener('click', ()=>{
+      navbar.classList.toggle('expander')
+
+      bodypadding.classList.toggle('body-pd')
+    })
+  }
+}
+showMenu('nav-toggle','navbar','body-pd')
+
+/*===== LINK ACTIVE  =====*/ 
+const linkColor = document.querySelectorAll('.nav__link')
+function colorLink(){
+  linkColor.forEach(l=> l.classList.remove('active'))
+  this.classList.add('active')
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+
+/*===== COLLAPSE MENU  =====*/ 
+const linkCollapse = document.getElementsByClassName('collapse__link')
+var i
+
+for(i=0;i<linkCollapse.length;i++){
+  linkCollapse[i].addEventListener('click', function(){
+    const collapseMenu = this.nextElementSibling
+    collapseMenu.classList.toggle('showCollapse')
+
+    const rotate = collapseMenu.previousElementSibling
+    rotate.classList.toggle('rotate')
+  })
+}
+
+
+/*===== MODAL LOGIN  =====*/ 
+function modal(e) {
+	let modal = document.createElement("div"), block = e.cloneNode(true);
+
+	block.id = "modalBlockLogin";
+    block.querySelector("#reg").id = "reg_form";
+	block.querySelector("#login").id = "login_form";
+
+	Object.assign(modal.style, {
+		"z-index": "1000",
+		position: "fixed",
+		left: "0px",
+		top: "0px",
+		width: "100%",
+		height: "100%",
+		backgroundColor: "rgba(0,0,0,.5)"
+	});
+
+	Object.assign(block.style, {
+		display: "initial",
+		opacity: 0,
+		position: "absolute",
+		left: "50%",
+		top: "50%",
+		transform: "translate(-50%, calc(-50% + 100px))",
+		transition: ".6s opacity, .6s transform"
+	});
+
+	modal.appendChild(block);
+
+	document.body.appendChild(modal);
+
+	setTimeout(()=>Object.assign(block.style, {
+		opacity: 1,
+		transform: "translate(-50%,-50%)"
+	}),15);
+
+	modal.addEventListener("click", e => e.target === modal ? document.body.removeChild(modal) : "");
+}
