b2c5f4f0ddedd322082b989d61eed06b7345a727
Vladyslav <vladyslav.oleksenko@nure.ua>
2021-10-19 19:32:27 +0300

    add login/reg requests

--- Website/assets/js/forms.js
+++ Website/assets/js/forms.js
@@ -13,7 +13,7 @@ function login_validation(){
       //Ок
       error_email = 1;
     } else {
-        if(email.length == 0){
+        if(email.length === 0){
             document.getElementById("modal_error_login_email").textContent = "Введіть пошту.";
         }else{
             document.getElementById("modal_error_login_email").textContent = "Невірний адрес пошти.";
@@ -24,7 +24,7 @@ function login_validation(){
     //Валидация пароля
     var rep = /^[a-zA-Z0-9]+$/;
     if (rep.test(String(pass).toLowerCase())) {
-        if(pass.length == 0){
+        if(pass.length === 0){
             document.getElementById("modal_error_login_pass").textContent = "Введіть пароль.";
         }else{
             if(pass.length < 8){
@@ -32,10 +32,10 @@ function login_validation(){
             }else{
                 error_pass = 1;
             }
-            
+
         }
       } else {
-          if(pass.length == 0){
+          if(pass.length === 0){
               document.getElementById("modal_error_login_pass").textContent = "Введіть пароль.";
           }else{
               document.getElementById("modal_error_login_pass").textContent = "Тільки латинськи літери";
@@ -43,8 +43,18 @@ function login_validation(){
           error_pass = 0;
     }
 
-    if(error_email == 1 && error_pass == 1){
-        alert("Login done");
+    if(error_email === 1 && error_pass === 1){
+        console.log("Login done. Server response:");
+
+        const requestUrl = "api/users/login"
+        const requestBody = {
+            email: email,
+            pass: pass
+        }
+
+        ServerRequest.send("POST", requestUrl, requestBody)
+          .then(data => console.log(data))
+          .catch(err => console.log(err));
     }
 }
 
@@ -56,7 +66,7 @@ function reg_validation(){
     //Валидация имени
     var repsa = /^[a-zA-Z0-9]+$/;
     if (repsa.test(String(name).toLowerCase())) {
-        if(name.length == 0){
+        if(name.length === 0){
             document.getElementById("modal_error_reg_name").textContent = "Введіть логін.";
         }else{
             if(name.length < 6){
@@ -67,10 +77,10 @@ function reg_validation(){
             }else{
                 error_name = 1;
             }
-            
+
         }
       } else {
-          if(name.length == 0){
+          if(name.length === 0){
               document.getElementById("modal_error_reg_name").textContent = "Введіть логін.";
           }else{
               document.getElementById("modal_error_reg_name").textContent = "Тільки латинськи літери";
@@ -85,7 +95,7 @@ function reg_validation(){
       //Ок
       error_email = 1;
     } else {
-        if(email.length == 0){
+        if(email.length === 0){
             document.getElementById("modal_error_reg_email").textContent = "Введіть пошту.";
         }else{
             document.getElementById("modal_error_reg_email").textContent = "Невірний адрес пошти.";
@@ -95,7 +105,7 @@ function reg_validation(){
 
     //Валидация пароля
     if (rep.test(String(pass).toLowerCase())) {
-        if(pass.length == 0){
+        if(pass.length === 0){
             document.getElementById("modal_error_ref_pass").textContent = "Введіть пароль.";
         }else{
             if(pass.length < 8){
@@ -103,10 +113,10 @@ function reg_validation(){
             }else{
                 error_pass = 1;
             }
-            
+
         }
       } else {
-          if(pass.length == 0){
+          if(pass.length === 0){
               document.getElementById("modal_error_ref_pass").textContent = "Введіть пароль.";
           }else{
               document.getElementById("modal_error_ref_pass").textContent = "Тільки латинськи літери";
@@ -114,7 +124,18 @@ function reg_validation(){
           error_pass = 0;
     }
 
-    if(error_name == 1 && error_email == 1 && error_pass == 1){
-        alert("Reg done");
+    if(error_name === 1 && error_email === 1 && error_pass === 1){
+        console.log("Reg done. Server response:");
+
+        const requestUrl = "api/users/reg"
+        const requestBody = {
+            name: name,
+            email: email,
+            pass: pass
+        }
+
+        ServerRequest.send("POST", requestUrl, requestBody)
+          .then(data => console.log(data))
+          .catch(err => console.log(err));
     }
 }
