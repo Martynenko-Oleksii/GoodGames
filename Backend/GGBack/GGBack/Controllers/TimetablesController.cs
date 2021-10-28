6bfb34f3d2529aa11c1e80bb7084c7e6fec24dd0
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-10-28 17:37:40 +0300

    Add method to generate schedule for singleplayer sports

--- Backend/GGBack/GGBack/Controllers/TimetablesController.cs
+++ Backend/GGBack/GGBack/Controllers/TimetablesController.cs
@@ -64,10 +64,37 @@ namespace GGBack.Controllers
             DateTime startTime = timeBoundary.Start;
             DateTime endTime = timeBoundary.End;
 
+            if (competitorsCount < minCompetitorsCount)
+            {
+                return BadRequest("Wrong competitors count");
+            }
+
+            if (teamsCount % 2 != 0)
+            {
+                return BadRequest("teams count % 2 != 0");
+            }
+
+            if (timeBoundary.End.Hour - timeBoundary.Start.Hour < 2)
+            {
+                return BadRequest("Wrong time for games");
+            }
+
+            foreach (string team in teams)
+            {
+                int competitorsCountPerTeam = competitors
+                    .Where(c => c.Name.Equals(team))
+                    .ToList().Count;
+
+                if (competitorsCountPerTeam != teamSize)
+                {
+                    return BadRequest($"Wrong competitors count in team {team}");
+                }
+            }
+
+            List<TimetableCell> cells = new List<TimetableCell>();
             if (hasTeam)
             {
-                if (competitorsCount % teamSize != 0 || 
-                    competitorsCount < minCompetitorsCount)
+                if (competitorsCount % teamSize != 0)
                 {
                     return BadRequest("Wrong competitors count");
                 }
@@ -77,51 +104,30 @@ namespace GGBack.Controllers
                     return BadRequest("Wrong teams count");
                 }
 
-                if (teamsCount % 2 != 0)
-                {
-                    return BadRequest("teams count % 2 != 0");
-                }
-
                 if (competitorsCount - teamsCount * teamSize != 0)
                 {
                     return BadRequest("Wrong competitors count per team");
                 }
 
-                foreach (string team in teams)
-                {
-                    int competitorsCountPerTeam = competitors
-                        .Where(c => c.Name.Equals(team))
-                        .ToList().Count;
-
-                    if (competitorsCountPerTeam != teamSize)
-                    {
-                        return BadRequest($"Wrong competitors count in team {team}");
-                    }
-                }
-
-                if (timeBoundary.End.Hour - timeBoundary.Start.Hour < 2)
-                {
-                    return BadRequest("Wrong time for games");
-                }
-
-                List<TimetableCell> cells = ScheduleGenerator
-                    .GenerateForTeamSports(teams, 
-                        competitors, competition, 
-                        startDate, endDate, 
-                        startTime, endTime);
-
-                if (cells == null)
-                {
-                    return BadRequest("Wront end date for competition");
-                }
-
-                context.TimetableCells.AddRange(cells);
+                cells = ScheduleGenerator.GenerateForTeamSports(teams, 
+                                            competitors, competition, 
+                                            startDate, endDate, 
+                                            startTime, endTime);
             }
             else
             {
-
+                cells = ScheduleGenerator.GenerateForNoTeamSports(teams,
+                                            competitors, competition,
+                                            startDate, endDate,
+                                            startTime, endTime);
             }
 
+            if (cells == null)
+            {
+                return BadRequest("Error :(");
+            }
+
+            context.TimetableCells.AddRange(cells);
             await context.SaveChangesAsync();
 
             return await context.TimetableCells.ToListAsync();
--- Backend/GGBack/GGBack/Utils/ScheduleGenerator.cs
+++ Backend/GGBack/GGBack/Utils/ScheduleGenerator.cs
@@ -17,7 +17,7 @@ namespace GGBack.Utils
 
             if (teams.Count % 4 == 0)
             {
-                //TODO
+                //TODO: if need
             }
             else
             {
@@ -72,6 +72,103 @@ namespace GGBack.Utils
                     {
                         DateTime = dt,
                         Competitors = competitorsPerTwoTeams,
+                        Competition = competition,
+                        GridStage = 1
+                    };
+
+                    timetableCells.Add(ttCell);
+                }
+            }
+
+            return timetableCells;
+        }
+
+        public static List<TimetableCell> GenerateForNoTeamSports(List<string> teams,
+            List<Competitor> competitors, Competition competition,
+            DateTime startDate, DateTime endDate,
+            DateTime startTime, DateTime endTime)
+        {
+            List<TimetableCell> timetableCells = new List<TimetableCell>();
+
+            Dictionary<DateTime, int> times = new Dictionary<DateTime, int>();
+            string[] teamsArray = teams.ToArray();
+            for (int i = 0; i < teams.Count; i += 2)
+            {
+                List<Competitor> competitorsTeamOne = competitors
+                    .Where(c => c.Team.Equals(teamsArray[i]))
+                    .ToList();
+
+                List<Competitor> competitorsTeamTwo = competitors
+                    .Where(c => c.Team.Equals(teamsArray[i + 1]))
+                    .ToList();
+
+                if (competitorsTeamOne.Count != competitorsTeamOne.Count)
+                {
+                    return null;
+                }
+
+                for (int j = 0; j < competitorsTeamOne.Count; j++)
+                {
+                    DateTime dt = new DateTime();
+
+                    if (DateTime.UtcNow > competition.StartDate)
+                    {
+                        DateTime temp = DateTime.UtcNow.AddDays(1);
+                        times.Add(new DateTime(temp.Year, temp.Month, temp.Day,
+                            startTime.Hour, startTime.Minute, 0), 0);
+                    }
+                    else
+                    {
+                        times.Add(new DateTime(startDate.Year, startDate.Month, startDate.Day,
+                            startTime.Hour, startTime.Minute, 0), 0);
+                    }
+
+                    KeyValuePair<DateTime, int> timePair = times.Last();
+                    DateTime newDateTime = new DateTime();
+                    if (timePair.Value < 3)
+                    {
+                        newDateTime = timePair.Key;
+                        times[newDateTime] += 1;
+                    }
+                    else
+                    {
+                        newDateTime = timePair.Key.AddHours(2);
+                    }
+
+                    if (newDateTime < endDate.AddDays(1))
+                    {
+                        if (newDateTime < endTime)
+                        {
+                            dt = newDateTime;
+
+                            if (!times.ContainsKey(newDateTime))
+                            {
+                                times.Add(newDateTime, 1);
+                            }
+                        }
+                        else
+                        {
+                            DateTime temp = newDateTime.AddDays(1);
+                            temp = new DateTime(temp.Year, temp.Month, temp.Day,
+                                startTime.Hour, startTime.Minute, 0);
+
+                            dt = temp;
+                            times.Add(temp, 1);
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
+                    List<Competitor> competitorsPerCell = new List<Competitor>();
+                    competitorsPerCell.Add(competitorsTeamOne.ElementAt(j));
+                    competitorsPerCell.Add(competitorsTeamTwo.ElementAt(j));
+
+                    TimetableCell ttCell = new TimetableCell
+                    {
+                        DateTime = dt,
+                        Competitors = competitorsPerCell,
                         Competition = competition,
                         GridStage = 1
                     };
