8ad1aee9650d4f60c76553181c6b85c58ebf7f1c
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-10-28 18:29:38 +0300

    Add a generation of teams by grid cells

--- Backend/GGBack/GGBack/Controllers/GridsController.cs
+++ Backend/GGBack/GGBack/Controllers/GridsController.cs
@@ -1,9 +1,12 @@
 ﻿using GGBack.Data;
+using GGBack.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GGBack.Controllers
@@ -20,5 +23,40 @@ namespace GGBack.Controllers
         }
 
         [HttpGet("{competitionId}")]
+        public ActionResult<string> Get(int competitionId)
+        {
+            List<TimetableCell> cells = context.TimetableCells
+                .Include(t => t.Competitors)
+                .Include(t => t.Competition)
+                .Include(t => t.WinResult)
+                .Where(t => t.Competition.Id == competitionId)
+                .ToList();
+
+            StringBuilder teams = new StringBuilder("\"teams\":[],");
+            StringBuilder results = new StringBuilder("\"results\":[]");
+
+            for (int i = 0; i < cells.Where(c => c.GridStage == 1).ToList().Count; i++)
+            {
+                string[] teamsArray = cells.Where(c => c.GridStage == 1)
+                    .ElementAt(i).Competitors
+                    .Select(c => c.Team).Distinct()
+                    .ToArray();
+
+                string gridCell = "[\"" + teamsArray[0] + "\", " + teamsArray[1] + "\"],";
+                teams.Insert(teams.Length-1, gridCell);
+            }
+
+            for (int i = 0; i < cells.Count; i++)
+            {
+                if (cells.ElementAt(i).WinResult != null)
+                {
+                    //TODO
+                }
+            }
+
+            StringBuilder tournamentGrid = new StringBuilder();
+
+            return tournamentGrid.Append(teams).Append(results).ToString();
+        }
     }
 }
