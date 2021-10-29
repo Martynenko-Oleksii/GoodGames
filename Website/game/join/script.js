81d6732464ba39100d2fad493f94504670079f15
Stamir36 <stanislav.miroshnichenko@nure.ua>
2021-10-29 21:13:59 +0300

    JS Join

--- Website/game/join/script.js
+++ Website/game/join/script.js
@@ -1,3 +1,31 @@
+/*! =========================================================
+ * =========================================================
+ *
+ *                       _oo0oo_
+ *                      o8888888o
+ *                      88" . "88
+ *                      (| -_- |)
+ *                      0\  =  /0
+ *                    ___/`---'\___
+ *                  .' \|     |// '.
+ *                 / \|||  :  |||// \
+ *                / _||||| -:- |||||- \
+ *               |   | \\  -  /// |   |
+ *               | \_|  ''\---/''  |_/ |
+ *               \  .-\__  '-'  ___/-. /
+ *             ___'. .'  /--.--\  `. .'___
+ *          ."" '<  `.___\_<|>_/___.' >' "".
+ *         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *         \  \ `_.   \_ __\ /__ _/   .-` /  /
+ *     =====`-.____`.___ \_____/___.-`___.-'=====
+ *                       `=---='
+ *
+ *     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~s
+ *
+ *               Будда говорит:  "Нет багов!"
+ *
+ * ========================================================= */
+
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
 
@@ -15,13 +43,30 @@ function getUrlVars() {
 var id = getUrlVars()["id"]; // Идентификатор пользователя
 var game = getUrlVars()["game"]; // Идентификатор соревнования
 
-form.onsubmit = (e)=>{
-  e.preventDefault();
+function sendjoin(){
   statusTxt.style.color = "#0D6EFD";
   statusTxt.style.display = "block";
   statusTxt.innerText = "Реєструємо вас...";
   form.classList.add("disabled");
 
   // Запрос...
-  
+  const requestUrl = "/api/";
+
+
+  const team = document.querySelector("#team").value;
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const age = document.querySelector("#age").value;
+  const weigth = document.querySelector("#weigth").value;
+  const gender = document.querySelector("#gender").value;
+  const healthstate = document.querySelector("#healthstate").value;
+  const competitions = getUrlVars()["game"];
+
+  const userId = game;
+    if (!userId) {
+        console.log("Соревнования нет.");
+        //return;
+    }
+
+  alert(team + "\n" + name + "\n" + email + "\n" + age + "\n" + weigth + "\n" +gender + "\n" +healthstate);
 }
