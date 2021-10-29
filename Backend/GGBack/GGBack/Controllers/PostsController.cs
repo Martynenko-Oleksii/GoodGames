ab4da045842b44ec234b68f054ea103b7f9c7cd1
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-10-29 18:34:35 +0300

    Fix controller for sending invitation

---
+++ Backend/GGBack/GGBack/Controllers/PostsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using GGBack.Utils;
+using GGBack.Data;
+using GGBack.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace GGBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        private ServerDbContext context;
+
+        public PostsController(ServerDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public ActionResult<string> PostNewInvition(Post post)
+        {
+            if (post.Equals(null))
+            {
+                return BadRequest("null");
+            }
+
+            if (!PostEmail.SendInvitation(post, context))
+            {
+                return BadRequest("mail not sent");
+            }
+
+            return Ok("ok");
+        }
+    }
+}
---
+++ Backend/GGBack/GGBack/Utils/PostEmail.cs
@@ -0,0 +1,60 @@
+﻿using GGBack.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace GGBack.Utils
+{
+    public class PostEmail
+    {
+        public static bool SendInvitation(Post post, ServerDbContext context)
+        {
+            try
+            {
+                string fromPost = context.Users
+                    .Where(u => u.Id == post.UserId)
+                    .Select(u => u.Email)
+                    .FirstOrDefault();
+                string fromName = context.Users
+                    .Where(u => u.Id == post.UserId)
+                    .Select(u => u.Login)
+                    .FirstOrDefault(); ;
+
+                MailAddress from = new MailAddress(fromPost, fromName);
+
+                MailAddress to = new MailAddress(post.Email);
+                MailMessage message = new MailMessage(from, to);
+
+                message.Subject = "GoodGames приглашает вас принять участие в соревновании";
+
+                message.Body = $"Пользователь под ником {fromName} приглашает вас" +
+                    $" присоедениться к турниру {context.Competitions.Find(post.CompetitionId).Title}," +
+                    $" время начала - {context.Competitions.Find(post.CompetitionId).StartDate}. /ссылка/";
+
+                message.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(fromPost, "mineworker");
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class Post
+    {
+        public int UserId { get; set; }
+
+        public int CompetitionId { get; set; }
+
+        public string Email { get; set; }
+    }
+}
