4869ecb0dcf74c4cdff2725422dc9c9198a9bd6a
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-01 10:21:05 +0200

    create method to generate mail body

--- Backend/GGBack/GGBack/Utils/PostEmail.cs
+++ Backend/GGBack/GGBack/Utils/PostEmail.cs
@@ -1,16 +1,21 @@
 ﻿using GGBack.Data;
+using GGBack.Models;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GGBack.Utils
 {
     public class PostEmail
     {
-        public static string SendInvitation(Post post, ServerDbContext context)
+        public static string SendInvitation(Post post, 
+            ServerDbContext context, IWebHostEnvironment env)
         {
             try
             {
@@ -19,20 +24,25 @@ namespace GGBack.Utils
                     .Where(u => u.Id == post.UserId)
                     .Select(u => u.Login)
                     .FirstOrDefault();
-                //string fromName = "Dummy";
 
                 MailAddress from = new MailAddress(fromPost, fromName);
-
                 MailAddress to = new MailAddress(post.Email);
+
                 MailMessage message = new MailMessage(from, to);
 
-                message.Subject = "GoodGames приглашает вас принять участие в соревновании";
+                message.Subject = "GoodGames запрошує вас прийняти участь у змаганні";
 
-                message.Body = $"Пользователь под ником {fromName} приглашает вас" +
-                    $" присоедениться к турниру {context.Competitions.Find(post.CompetitionId).Title}," +
-                    $" время начала - {context.Competitions.Find(post.CompetitionId).StartDate}. /ссылка/";
+                Competition competition =
+                    context.Competitions.Find(post.CompetitionId);
+                string body = CreateBody(env, fromName, competition);
 
-                message.IsBodyHtml = false;
+                if (body == null)
+                {
+                    return "CreateBody error";
+                }
+
+                message.Body = body;
+                message.IsBodyHtml = true;
                 SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
                 smtp.Credentials = new NetworkCredential(fromPost, "frick4224");
                 smtp.EnableSsl = true;
@@ -44,6 +54,37 @@ namespace GGBack.Utils
             }
 
             return "true";
+        }
+
+        private static string CreateBody(IWebHostEnvironment env, 
+            string login, Competition competition)
+        {
+            string path = env.WebRootPath;
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            if (!dirInfo.Exists)
+            {
+                return null;
+            }
+
+            string bodyFromFile;
+            using (FileStream fs = File.OpenRead($"{path}\\mail\\mail.txt"))
+            {
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length);
+                bodyFromFile = Encoding.Default.GetString(array);
+            }
+
+            StringBuilder body = new StringBuilder(bodyFromFile);
+            string url = "https://goodgames.kh.ua/game/join/?game=" + competition.Id;
+            body.Replace("{userLogin}", login);
+            body.Replace("{competitionTitle}", competition.Title);
+            body.Replace("{competitionCity}", competition.City);
+            body.Replace("{startDate}", competition.StartDate.ToString("D"));
+            body.Replace("{endDate}", competition.EndDate.ToString("D"));
+            body.Replace("{url}", url);
+
+            return body.ToString();
         }
     }
 
