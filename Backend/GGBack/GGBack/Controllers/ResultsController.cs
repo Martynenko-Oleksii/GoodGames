e593ecc0341f2fc9d4ef9d8487d9337ac9d5bc11
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-01 17:43:23 +0200

    Add controller with POST method to set results

---
+++ Backend/GGBack/GGBack/Controllers/ResultsController.cs
@@ -0,0 +1,51 @@
+﻿using GGBack.Data;
+using GGBack.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResultsController : ControllerBase
+    {
+        ServerDbContext context;
+
+        public ResultsController(ServerDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(WinResult winResult)
+        {
+            if (winResult == null)
+            {
+                return BadRequest();
+            }
+
+            TimetableCell cell = context.TimetableCells.Find(winResult.Id);
+
+            if (cell == null)
+            {
+                return BadRequest("Wrong id");
+            }
+
+            WinResult actualResult = new WinResult
+            {
+                Score = winResult.Score
+            };
+            context.WinResults.Add(actualResult);
+            cell.WinResult = actualResult;
+            await context.SaveChangesAsync();
+
+
+
+            return Ok();
+        }
+    }
+}
