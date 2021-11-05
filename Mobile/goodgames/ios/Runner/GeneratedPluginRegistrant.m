ea601ae15d4653a1cae7383f113624ceb96f4aaf
nik00045 <niknos09133456789@gmail.com>
2021-11-05 16:43:47 +0200

    1

--- Mobile/goodgames/ios/Runner/GeneratedPluginRegistrant.m
+++ Mobile/goodgames/ios/Runner/GeneratedPluginRegistrant.m
@@ -18,11 +18,18 @@
 @import video_player;
 #endif
 
+#if __has_include(<webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>)
+#import <webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>
+#else
+@import webview_flutter_wkwebview;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
+  [FLTWebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTWebViewFlutterPlugin"]];
 }
 
 @end
