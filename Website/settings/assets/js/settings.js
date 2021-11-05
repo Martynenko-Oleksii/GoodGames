99d46588b06d27e6b50ee288052e96893f91d812
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-05 09:51:31 +0200

    requests for changing login & email

--- Website/settings/assets/js/settings.js
+++ Website/settings/assets/js/settings.js
@@ -1,6 +1,9 @@
 const loginInputEl = document.querySelector(".login-input");
 const emailInputEl = document.querySelector(".email-input");
 
+const changeProfileInfoButtonEl =
+  document.querySelector(".change-profile-info-button");
+
 
 document.addEventListener("DOMContentLoaded", pageLoaded);
 
@@ -10,4 +13,47 @@ function pageLoaded() {
 
   loginInputEl.value = oldLogin;
   emailInputEl.value = oldEmail;
+}
+
+
+changeProfileInfoButtonEl.addEventListener("click", () => changeProfileInfo());
+
+function changeProfileInfo() {
+  const userId = Cookies.get("id");
+  if (!userId) {
+    console.log("Can`t get id from Cookies");
+    return;
+  }
+
+  
+  sendLoginChangeRequest();
+  sendEmailChangeRequest();
+
+  function sendLoginChangeRequest() {
+    const requestUrl = "/api/users/change/login";
+
+    const body = {
+      id: userId,
+      login: loginInputEl.value,
+    }
+
+    sendRequest(requestUrl, body);
+  }
+
+  function sendEmailChangeRequest() {
+    const requestUrl = "/api/users/change/email";
+
+    const body = {
+      id: userId,
+      email: emailInputEl.value,
+    }
+
+    sendRequest(requestUrl, body);
+  }
+
+  function sendRequest(requestUrl, body) {
+    ServerRequest.send("POST", requestUrl, body)
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
 }
