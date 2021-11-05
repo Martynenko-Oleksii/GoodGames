9b797ca81f750bd523d34e9ec8a825dd10aab11f
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-05 11:00:27 +0200

    update avatar image and buttons` text

--- Website/settings/assets/js/settings.js
+++ Website/settings/assets/js/settings.js
@@ -1,6 +1,8 @@
+const avatarEl = document.querySelector(".avatar");
+const changeAvatarButtonEl = document.querySelector(".change-avatar-button");
+const resetFileButtonEl = document.querySelector(".reset-file-button");
 const loginInputEl = document.querySelector(".login-input");
 const emailInputEl = document.querySelector(".email-input");
-
 const changeProfileInfoButtonEl =
   document.querySelector(".change-profile-info-button");
 
@@ -8,11 +10,38 @@ const changeProfileInfoButtonEl =
 document.addEventListener("DOMContentLoaded", pageLoaded);
 
 function pageLoaded() {
+  updateAvatarInterface();
+
   const oldLogin = Cookies.get("login");
   const oldEmail = Cookies.get("email");
 
   loginInputEl.value = oldLogin;
   emailInputEl.value = oldEmail;
+}
+
+function updateAvatarInterface() {
+  const userId = Cookies.get("id");
+  if (!userId) {
+    console.log("Can`t get id from Cookies");
+    return;
+  }
+
+  ServerRequest.send("POST", "/api/users/" + userId)
+    .then(data => {
+      const avatarPath = data.avatarPath;
+
+      if (avatarPath) {
+        changeAvatarButtonEl.innerHTML = "Змінити аватар";
+        avatarEl.src = avatarPath;
+        resetFileButtonEl.style.display = "inline-block";
+        return;
+      }
+
+      changeAvatarButtonEl.innerHTML = "Додати аватар";
+      avatarEl.src = "/assets/images/user-48.png";
+      resetFileButtonEl.style.display = "none";
+    })
+    .catch(err => console.log(err));
 }
 
 
