0d5eae08ef9d8fa834bba1f7056b37f96f011ddc
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-07 10:25:26 +0200

    Use migration

---
+++ Backend/GGBack/GGBack/Migrations/20211106213258_InitialCreate.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GGBack.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Competitors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Weigth = table.Column<int>(type: "int", nullable: false),
+                    HealthState = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Team = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competitors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MinCompetitorsCount = table.Column<int>(type: "int", nullable: false),
+                    HasTeam = table.Column<bool>(type: "bit", nullable: false),
+                    MinTeamsCount = table.Column<int>(type: "int", nullable: false),
+                    TeamSize = table.Column<int>(type: "int", nullable: false),
+                    HasGrid = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WinResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Score = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WinResults", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Competitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsOpen = table.Column<bool>(type: "bit", nullable: false),
+                    SportId = table.Column<int>(type: "int", nullable: true),
+                    AgeLimit = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsPublic = table.Column<bool>(type: "bit", nullable: false),
+                    StreamUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Competitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Competitions_Sports_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AvatarPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubscriptionId = table.Column<int>(type: "int", nullable: true),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Subscriptions_SubscriptionId",
+                        column: x => x.SubscriptionId,
+                        principalTable: "Subscriptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompetitionCompetitor",
+                columns: table => new
+                {
+                    CompetitionsId = table.Column<int>(type: "int", nullable: false),
+                    CompetitorsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompetitionCompetitor", x => new { x.CompetitionsId, x.CompetitorsId });
+                    table.ForeignKey(
+                        name: "FK_CompetitionCompetitor_Competitions_CompetitionsId",
+                        column: x => x.CompetitionsId,
+                        principalTable: "Competitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompetitionCompetitor_Competitors_CompetitorsId",
+                        column: x => x.CompetitorsId,
+                        principalTable: "Competitors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimetableCells",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompetitionId = table.Column<int>(type: "int", nullable: true),
+                    GridStage = table.Column<int>(type: "int", nullable: false),
+                    WinResultId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimetableCells", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimetableCells_Competitions_CompetitionId",
+                        column: x => x.CompetitionId,
+                        principalTable: "Competitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TimetableCells_WinResults_WinResultId",
+                        column: x => x.WinResultId,
+                        principalTable: "WinResults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompetitionUser",
+                columns: table => new
+                {
+                    CompetitionsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompetitionUser", x => new { x.CompetitionsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_CompetitionUser_Competitions_CompetitionsId",
+                        column: x => x.CompetitionsId,
+                        principalTable: "Competitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompetitionUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SportUser",
+                columns: table => new
+                {
+                    SportsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SportUser", x => new { x.SportsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_SportUser_Sports_SportsId",
+                        column: x => x.SportsId,
+                        principalTable: "Sports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SportUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompetitorTimetableCell",
+                columns: table => new
+                {
+                    CompetitorsId = table.Column<int>(type: "int", nullable: false),
+                    TimetableCellsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompetitorTimetableCell", x => new { x.CompetitorsId, x.TimetableCellsId });
+                    table.ForeignKey(
+                        name: "FK_CompetitorTimetableCell_Competitors_CompetitorsId",
+                        column: x => x.CompetitorsId,
+                        principalTable: "Competitors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompetitorTimetableCell_TimetableCells_TimetableCellsId",
+                        column: x => x.TimetableCellsId,
+                        principalTable: "TimetableCells",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetitionCompetitor_CompetitorsId",
+                table: "CompetitionCompetitor",
+                column: "CompetitorsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Competitions_SportId",
+                table: "Competitions",
+                column: "SportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetitionUser_UsersId",
+                table: "CompetitionUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetitorTimetableCell_TimetableCellsId",
+                table: "CompetitorTimetableCell",
+                column: "TimetableCellsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportUser_UsersId",
+                table: "SportUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimetableCells_CompetitionId",
+                table: "TimetableCells",
+                column: "CompetitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimetableCells_WinResultId",
+                table: "TimetableCells",
+                column: "WinResultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SubscriptionId",
+                table: "Users",
+                column: "SubscriptionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompetitionCompetitor");
+
+            migrationBuilder.DropTable(
+                name: "CompetitionUser");
+
+            migrationBuilder.DropTable(
+                name: "CompetitorTimetableCell");
+
+            migrationBuilder.DropTable(
+                name: "SportUser");
+
+            migrationBuilder.DropTable(
+                name: "Competitors");
+
+            migrationBuilder.DropTable(
+                name: "TimetableCells");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Competitions");
+
+            migrationBuilder.DropTable(
+                name: "WinResults");
+
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+
+            migrationBuilder.DropTable(
+                name: "Sports");
+        }
+    }
+}
