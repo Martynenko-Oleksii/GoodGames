f7ae7843e8cc93495bb55694dc22363ce929d151
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-08 15:11:53 +0200

    some code refactor

--- Website/game/assets/js/game.js
+++ Website/game/assets/js/game.js
@@ -47,18 +47,16 @@ function updateGeneralCompetitionInfo() {
         document.querySelector(".competition-title").innerHTML = info.title;
         document.querySelector(".organizer").innerHTML = info.user.login;
         document.querySelector(".city").innerHTML = info.city;
-        if(info.state == 0 || info.state == "0"){
+        info.state = parseInt(info.state);
+        if (info.state === 0) {
             document.querySelector(".state_sport").innerHTML = "Планування та набір";
-        }
-        if(info.state == 1 || info.state == "1"){
+        }else if (info.state === 1) {
             document.querySelector(".state_sport").innerHTML = "Проводиться";
             document.getElementById('start_competitions').remove();
-        }
-        if(info.state == 2 || info.state == "2"){
+        } else if (info.state === 2) {
             document.querySelector(".state_sport").innerHTML = "Завершено";
             document.getElementById('start_competitions').remove();
-        }
-        if(info.state != 0 && info.state != 1 && info.state != 2){
+        } else if (info.state !== 0 && info.state !== 1 && info.state !== 2) {
             document.querySelector(".state_sport").innerHTML = "Статус не визначено.";
         }
         document.querySelector(".competition-description").innerHTML =
@@ -71,8 +69,6 @@ function updateGeneralCompetitionInfo() {
             if (!info.isOpen) {
                 document.getElementById('send_add_player').remove();
             }
-        } else {
-
         }
 
         //Рассписание
