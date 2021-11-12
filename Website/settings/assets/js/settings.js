84290ef10a74868a2ee32367c1aa620f49c710a9
VladyslavOleksenko <vladyslav.oleksenko@nure.ua>
2021-11-12 09:48:34 +0200

    Update settings.js
    fix variable name error

--- Website/settings/assets/js/settings.js
+++ Website/settings/assets/js/settings.js
@@ -120,7 +120,7 @@ function updateFavouriteSportKinds() {
         </div>
         `;
 
-      sportKindContainerEl.appendChild(sportKind);
+      sportKindContainerEl.appendChild(sportKindEl);
     }
   }
 }
