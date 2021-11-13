21832aecca2cdd161ce404c6172c77e7831d5197
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-13 18:05:45 +0200

    Initialize methods to assemble news

---
+++ Backend/GGBack/GGBack/Utils/NewsAssembler.cs
@@ -0,0 +1,63 @@
+﻿using GGBack.Data;
+using GGBack.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGBack.Utils
+{
+    public class NewsAssembler
+    {
+        public static IEnumerable<News> AssembleNews(ServerDbContext context, User user = null)
+        {
+            if (user == null)
+            {
+                return AssembleAllNewsHeader(context.RawNewss.ToList());
+            }
+            else
+            {
+                return AssembleAllNewsHeader(context.RawNewss
+                    .Include(rn => rn.Competition)
+                        .ThenInclude(c => c.Sport)
+                    .Where(rn => user.Sports.Contains(rn.Competition.Sport))
+                    .ToList());
+
+            }
+        }
+
+        public static News AssembleFullNews(RawNews rawNews)
+        {
+            return new News
+            {
+                Id = rawNews.Id,
+                Header = AssembleNewsHeader(rawNews),
+                Body = AssembleNewsBody(rawNews),
+                Date = rawNews.Date,
+                CompetitionTitle = rawNews.Competition.Title,
+                CompetitionId = rawNews.Competition.Id
+            };
+        }
+
+        private static IEnumerable<News> AssembleAllNewsHeader(List<RawNews> rawNews)
+        {
+            return null;
+        }
+
+        private static string AssembleNewsHeader(RawNews rawNews)
+        {
+            return null;
+        }
+
+        private static string AssembleNewsBody(RawNews rawNews)
+        {
+            return null;
+        }
+
+        private static string GetNewsTemplate(string path)
+        {
+            return null;
+        }
+    }
+}
