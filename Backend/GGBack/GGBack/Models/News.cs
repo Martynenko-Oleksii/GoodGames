17dc92eb2899ff34f2febb28436acf17e8b0578d
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-13 16:28:35 +0200

    Add classes for news models

---
+++ Backend/GGBack/GGBack/Models/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGBack.Models
+{
+    public class News
+    {
+        public string Header { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Date { get; set; }
+    
+        public string CompetitionTitle { get; set; }
+
+        public int CompetitionId { get; set; }
+    }
+}
---
+++ Backend/GGBack/GGBack/Models/RawNews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGBack.Models
+{
+    public class RawNews
+    {
+        public string HeaderData { get; set; }
+
+        public string BodyData { get; set; }
+    
+        public string TemplatePath { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Competition Competition { get; set; }
+    }
+}
