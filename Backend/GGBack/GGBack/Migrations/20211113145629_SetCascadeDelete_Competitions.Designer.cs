3eddc5c009d167da250c84e7382b60ea30f68310
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-13 16:59:07 +0200

    Creaate migration

--- Backend/GGBack/GGBack/Migrations/20211113145629_SetCascadeDelete_Competitions.Designer.cs
+++ Backend/GGBack/GGBack/Migrations/20211113145629_SetCascadeDelete_Competitions.Designer.cs
@@ -4,14 +4,16 @@ using GGBack.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GGBack.Migrations
 {
     [DbContext(typeof(ServerDbContext))]
-    partial class ServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211113145629_SetCascadeDelete_Competitions")]
+    partial class SetCascadeDelete_Competitions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
