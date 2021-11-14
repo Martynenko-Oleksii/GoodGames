8097018b8ad454143c479ab6c065c929fe691a2b
Мартиненко Олексій <oleksii.martynenko@nure.ua>
2021-11-14 13:42:36 +0200

    Add the method to generate raw news

---
+++ Backend/GGBack/GGBack/Utils/NewsGenerator.cs
@@ -0,0 +1,53 @@
+﻿using GGBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GGBack.Utils
+{
+    public enum NewsType
+    {
+        CompetitionCreating,
+        CompetitionStarting,
+        CompetitionEnding,
+        MatchStarting,
+        MatchEnding
+    }
+
+    public class NewsGenerator
+    {
+        public static RawNews SetRawNews(string[] headerData, string[] bodyData, Competition competition, NewsType type)
+        {
+            RawNews rawNews = new RawNews
+            {
+                HeaderData = String.Join(';', headerData),
+                BodyData = String.Join(';', bodyData),
+                Date = DateTime.UtcNow,
+                Competition = competition
+            };
+            switch (type)
+            {
+                case NewsType.CompetitionCreating:
+                    rawNews.TemplatePath = "\\competitionCreating.txt";
+                    break;
+                case NewsType.CompetitionStarting:
+                    rawNews.TemplatePath = "\\competitionStarting.txt";
+                    break;
+                case NewsType.CompetitionEnding:
+                    rawNews.TemplatePath = "\\competitionEnding.txt";
+                    break;
+                case NewsType.MatchStarting:
+                    rawNews.TemplatePath = "\\matchStarting.txt";
+                    break;
+                case NewsType.MatchEnding:
+                    rawNews.TemplatePath = "\\matchEnding.txt";
+                    break;
+                default:
+                    return null;
+            }
+
+            return rawNews;
+        }
+    }
+}
