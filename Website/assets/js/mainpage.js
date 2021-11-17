a234f22bb8a5c5f1fd9aab0af73f1d47333face8
Stamir36 <stanislav.miroshnichenko@nure.ua>
2021-11-17 19:45:04 +0200

    Get news main page

--- Website/assets/js/mainpage.js
+++ Website/assets/js/mainpage.js
@@ -58,6 +58,7 @@ function updateCompetitionList() {
           }
           maxcomplist++;
       }
+      getNewsFromServer();
     }
 
     function get_id_live(linkstream){
@@ -84,10 +85,86 @@ function updateCompetitionList() {
     }
 }
 
+const newsWrapperEl = document.querySelector(".news_list");
+function getNewsFromServer() {
+  sendServerRequest();
 
-// УДАЛИТЬ
-function login_test(){
-    Cookies.set("id", 1);
-    Cookies.set("login", "User System");
-    Cookies.set("email", "user@gg.ua")
+  function sendServerRequest() {
+    const requestParams = new RequestParams();
+    requestParams.url = "/api/news";
+    ServerRequest.send(requestParams)
+      .then(data => parseServerResponse(data))
+      .catch(err => console.log(err));
+  }
+
+  function parseServerResponse(serverResponse) {
+    newsWrapperEl.innerHTML = "";
+
+    if (!serverResponse || !serverResponse.length) {
+      newsWrapperEl.innerHTML =
+        `<article class="card" style="display: -webkit-box;">
+            <div style="display: block; width: 90%; margin-left: 10px; margin-right: 10px;">
+                <div class="card-header">
+                    <a>Новин немає.</a>
+                </div>
+            </div>
+        </article>`;
+      return;
+    }
+
+    serverResponse = serverResponse.reverse();
+    let maxcomplist = 0;
+    for (let oneNewsInfo of serverResponse) {
+      if(maxcomplist < 21){
+        parseOneNews(oneNewsInfo);
+      }else{
+        return;
+      }
+      maxcomplist++;
+    }
+
+    document.body.appendChild(scriptEl)
+  }
+
+  function parseOneNews(oneNewsInfo) {
+    const id = oneNewsInfo.id;
+    const header = oneNewsInfo.header;
+    const body = oneNewsInfo.body || "До події не доданий опис";
+
+    const months = ["січня", "лютого", "березня", "квітня", "травня", "червня", "липня", "серпня", "вересня", "жовтня", "листопада", "грудня"]
+    const date = new Date(oneNewsInfo.date);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+
+    const headerFirstWord = oneNewsInfo.header.split(":")[0].toLowerCase();
+    let imageUrl = "https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1537132205/news-slider/item-2.webp";
+    if (headerFirstWord === "створено") {
+      imageUrl = "news/image/new.png";
+    } else if (headerFirstWord === "розпочато") {
+      imageUrl = "news/image/start.png";
+    } else if (headerFirstWord === "завершено") {
+      imageUrl = "news/image/end.png";
+    }
+
+    newsWrapperEl.innerHTML +=
+      `<article class="card" style="display: -webkit-box;">
+          <div class="pc">
+              <img src="${imageUrl}" width="250" height="160" alt="" style="border-radius: 15px;">
+          </div>
+          <div style="display: block; width: 90%; margin-left: 10px; margin-right: 10px;">
+              <div class="card-header">
+                  <a href="#">${header}</a>
+              </div>
+              <div class="card-meta">
+                  <p>${body}</p>
+              </div>
+              <div class="card-footer">
+                  <div class="card-meta card-meta--date">
+                      <ion-icon name="calendar-outline" class="nav__icon" style="margin-right: 5px;"></ion-icon>
+                      ${day} ${month}
+                  </div>
+              </div>
+          </div>
+      </article>`;
+  }
 }
